94813616ee7e9bf1569299e74e97e960212d348f
Anchana-09 <anchanasivakumarnair@gmail.com>
2019-08-04 14:01:08 +0530

    Updates

---
+++ precache-manifest.965072614f4a5b21d37ed8386dedc64c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c91b990361324e13210af73db5a9176",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f75df331ddcdad35bd1b",
+    "url": "/monsters-rolodex/static/css/main.56777e17.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f75df331ddcdad35bd1b",
+    "url": "/monsters-rolodex/static/js/main.5e3f9c22.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
